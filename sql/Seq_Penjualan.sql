89651925ab3e7c428aa1488f69eb82e973a17c23
pinkycindy <pinkykyubby@gmail.com>
2017-12-10 19:01:49 +0700

    sequence untuk tabel di oracle

---
+++ sql/Seq_Penjualan.sql
@@ -0,0 +1,23 @@
+--------------------------------------------------------
+--  File created - Sunday-December-10-2017   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Sequence PENJUALAN_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "FPM"."PENJUALAN_SEQ"  MINVALUE 1 MAXVALUE 999999999999999999999999999 INCREMENT BY 1 START WITH 1 CACHE 20 NOORDER  NOCYCLE ;
+--------------------------------------------------------
+--  DDL for Sequence PRODUK_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "FPM"."PRODUK_SEQ"  MINVALUE 1 MAXVALUE 999999999999999999999999999 INCREMENT BY 1 START WITH 521 CACHE 20 NOORDER  NOCYCLE ;
+--------------------------------------------------------
+--  DDL for Sequence WAKTU_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "FPM"."WAKTU_SEQ"  MINVALUE 1 MAXVALUE 999999999999999999999999999 INCREMENT BY 1 START WITH 1 CACHE 20 NOORDER  NOCYCLE ;
+--------------------------------------------------------
+--  DDL for Sequence PELANGGAN_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "FPM"."PELANGGAN_SEQ"  MINVALUE 1 MAXVALUE 999999999999999999999999999 INCREMENT BY 1 START WITH 1 CACHE 20 NOORDER  NOCYCLE ;
